4ce795496f6a74286f7cdaeab3c14cafb556fadc
Claire Rosenthal <113315102+claire-therose@users.noreply.github.com>
2025-05-21 23:34:25 -0700

    reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,12 @@ import { color, motion, spring } from "motion/react";
 import CabnetLogo from "./ui_elements/cabnet_logo";
 import MenuButton from "./ui_elements/menu_button";
 import MenuPanel from "./ui_elements/menu_panel";
-import InformationPage from "./pages/information_page";
 
 export default function Home() {
   return (
     <div className="">
       {/* top bar spacing */}
       <div className="w-full h-11"></div> 
-      
-      <InformationPage />
 
       <div className="absolute top-0 w-full">
         <div className="absolute bg-linear-to-r/oklch from-cpink-400/70 md:via-pink-500/1 to-fuchsia-400/70 h-11 w-full mask-position-[left_top_426.5px] topology-background mask-size-[20rem]" />
