d6c79b991e84f18d0c603c6ecb71e36ecc41a396
Claire Rosenthal <113315102+claire-therose@users.noreply.github.com>
2025-11-13 11:04:20 -0800

    Updated Sign In Button
    Updated sign in button to make the link functional.
    Also updated it's font weight from medium to semibold.

--- src/app/ui_elements/signin_button.tsx
+++ src/app/ui_elements/signin_button.tsx
@@ -10,11 +10,12 @@ export default function SignInButton() {
     whileFocus="hover"
     animate="rest"
     className="h-11 px-3 relative">
-      <Link href={"/sign-in"}></Link>
-      <motion.div 
-          className="absolute h-0.75 bottom-0 inset-x-0 justify-center bg-cpink-400"
-          variants={highlight_variants_x} />
-      <div className="font-[family-name:var(--font-outfit)] font-medium text-xl text-white text-center">Sign In</div>
+      <Link href={"/sign-in"}>
+        <motion.div 
+            className="absolute h-0.75 bottom-0 inset-x-0 justify-center bg-cpink-400"
+            variants={highlight_variants_x} />
+        <div className="font-[family-name:var(--font-outfit)] font-semibold text-xl text-white text-center">Sign In</div>
+      </Link>
     </motion.button>
   )
 }
